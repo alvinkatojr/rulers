877853819bc73e98f947cfaef6d27b7685bec094
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-01-06 16:08:56 +0300

    Edits call method to retrieve a new controller and action for creation and sending respectively

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,6 +5,9 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
     end
   end
