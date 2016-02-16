4edd1c61a383094672c692048728a8adbb06eb2e
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-16 22:28:10 +0300

    Adds section for a redirect to the home controller and index action

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -14,6 +14,9 @@ module Rulers
         return [200, {'Content-Type' => 'text/html'}, [File.read "public/index.html"]]
         # Sets the home controller and index action
         # env={'PATH_INFO' => '/home/index.html'}
+
+        # Sets a redirect
+        # return [301, { 'Location' => '/home/index.html'}, []]
       end
 
       klass, act = get_controller_and_action(env)
