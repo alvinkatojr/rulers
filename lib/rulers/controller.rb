56146e6c845a35659ba160f9e2e1836410b550eb
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-02-26 01:16:39 +0300

    Adds controller_name method to the render method

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -10,7 +10,7 @@ class Controller
   end
 
   def render(view_name, locals = {})
-    filename = File.join "app", "views", "#{view_name}.html.erb"
+    filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
     template = File.read filename
     eruby = Erubis::Eruby.new(template)
     eruby.result(locals.merge(:env => env))
@@ -19,6 +19,5 @@ class Controller
   def controller_name
     klass = self.class
     klass = klass.to_s.gsub(/Controller$/, '')
-    Rulers.to_underscore klass
-  end
+    Rulers.to_underscor
 end
