62968f2a53d5580eafee021f97fccbc8567415c2
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-05-01 00:41:00 +0300

    Okay rubocop you can stop crying now...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require 'rack/test'
 require 'test/unit'
 
 # Always use local Rulers first
-d = File.join(File.dirname(__FILE__), "..", "lib")
+d = File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH.unshift File.expand_path(d)
 
-require "rulers"
+require 'rulers'
