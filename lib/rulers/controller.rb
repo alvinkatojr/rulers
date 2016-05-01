830e05251529124e69c2435b3f0b29d282e50e09
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-05-01 05:21:13 +0300

    Wraps the controller class into the rulers module

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,30 +1,32 @@
 require 'erubis'
 
-class Controller
-  attr_reader :env
+module Rulers
+  class Controller
+    attr_reader :env
 
-  def initialize(env)
-    @env = env
-  end
-
-  def instance_vars
-    vars = {}
-    instance_variables.each do |name|
-      vars[name[1..-1]] = instance_variable_get name.to_sym
+    def initialize(env)
+      @env = env
     end
-    vars
-  end
 
-  def render(view_name, locals = instance_vars)
-    filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
-    template = File.read filename
-    eruby = Erubis::Eruby.new(template)
-    eruby.result(locals.merge(:env => env))
-  end
+    def instance_vars
+      vars = {}
+      instance_variables.each do |name|
+        vars[name[1..-1]] = instance_variable_get name.to_sym
+      end
+      vars
+    end
 
-  def controller_name
-    klass = self.class
-    klass = klass.to_s.gsub(/Controller$/, '')
-    Rulers.to_underscor
+    def render(view_name, locals = instance_vars)
+      filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result(locals.merge(:env => env))
+    end
+
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub(/Controller$/, '')
+      Rulers.to_underscor
+    end
   end
 end
