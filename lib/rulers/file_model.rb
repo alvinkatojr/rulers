a03bd01ef530df6a37c3d1d8abd151293f2cf089
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-05-17 23:42:12 +0300

    Creates a new file in memory

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -45,6 +45,19 @@ module Rulers
         names = files.map { |f| f.split('/')[-1] }
         highest = names.map { |b| b.to_i }.max
         id = highest + 1
+
+        File.open("db/quotes/#{id}.json", "w") do |f|
+          f.write <<-TEMPLATE
+          {
+            "submitter": "#{hash["submitter"]}",
+            "quote": "#{hash["quote"]}",
+            "attribution": "#{hash["attribution"]}"
+          }
+          TEMPLATE
+        end
+
+
+        end
       end
     end
   end
