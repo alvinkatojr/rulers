ea170360c93067abda37d32fb62ab0902dc193af
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-05-19 01:33:59 +0300

    Refactors create method's map call to use an implicit block

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -43,7 +43,7 @@ module Rulers
 
         files = Dir['db/quotes/*.json']
         names = files.map { |f| f.split('/')[-1] }
-        highest = names.map { |b| b.to_i }.max
+        highest = names.map(&:to_i).max
         id = highest + 1
 
         File.open("db/quotes/#{id}.json", 'w') do |f|
